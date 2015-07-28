80628e8e8583774eeb6dc70e839c700cf27846cb
Dan Tillberg <dan@tillberg.us>
2015-07-28 16:04:31 -0400

    gofmt

--- main.go
+++ main.go
@@ -1,63 +1,63 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "os/signal"
-    "time"
-    "github.com/tillberg/ansi-log"
+	"fmt"
+	"github.com/tillberg/ansi-log"
+	"os"
+	"os/signal"
+	"time"
 )
 
 var colors []string = []string{"red", "yellow", "magenta", "cyan", "blue"}
+
 func writeStuff(num int, stop chan bool, done chan bool) {
-    i := 0
-    color := colors[num / 4]
-    out := log.New(os.Stderr, fmt.Sprintf("@(dim)[@(green:writer-%d)] ", num), log.Lelapsed)
-    format := fmt.Sprintf("@(dim:My number is) @(%s:%%d)", color)
-    for {
-        select {
-        case <- stop:
-            out.Close()
-            done <- true
-            return
-        default:
-            out.Replacef(format, i)
-            i += num
-            if i > 10000 {
-                i = 0
-                out.Println()
-            }
-        }
-        time.Sleep(time.Microsecond)
-    }
+	i := 0
+	color := colors[num/4]
+	out := log.New(os.Stderr, fmt.Sprintf("@(dim)[@(green:writer-%d)] ", num), log.Lelapsed)
+	format := fmt.Sprintf("@(dim:My number is) @(%s:%%d)", color)
+	for {
+		select {
+		case <-stop:
+			out.Close()
+			done <- true
+			return
+		default:
+			out.Replacef(format, i)
+			i += num
+			if i > 10000 {
+				i = 0
+				out.Println()
+			}
+		}
+		time.Sleep(time.Microsecond)
+	}
 }
 
 func main() {
-    log.EnableMultilineMode()
-    log.EnableColorTemplate()
-    stop := make(chan bool)
-    done := make(chan bool)
+	log.EnableMultilineMode()
+	log.EnableColorTemplate()
+	stop := make(chan bool)
+	done := make(chan bool)
 
-    numLines := 20
-    for n := 0; n < numLines; n++ {
-        go writeStuff(n, stop, done)
-    }
-    signalChan := make(chan os.Signal, 1)
-    cleanupDone := make(chan bool)
-    signal.Notify(signalChan, os.Interrupt)
-    go func() {
-        for _ = range signalChan {
-            log.Println("Received SIGINT. Shutting down...")
-            for n := 0; n < numLines; n++ {
-                stop <- true
-            }
-            for n := 0; n < numLines; n++ {
-                <- done
-            }
-            log.Println("Exiting...")
-            cleanupDone <- true
-        }
-    }()
-    <-cleanupDone
+	numLines := 20
+	for n := 0; n < numLines; n++ {
+		go writeStuff(n, stop, done)
+	}
+	signalChan := make(chan os.Signal, 1)
+	cleanupDone := make(chan bool)
+	signal.Notify(signalChan, os.Interrupt)
+	go func() {
+		for _ = range signalChan {
+			log.Println("Received SIGINT. Shutting down...")
+			for n := 0; n < numLines; n++ {
+				stop <- true
+			}
+			for n := 0; n < numLines; n++ {
+				<-done
+			}
+			log.Println("Exiting...")
+			cleanupDone <- true
+		}
+	}()
+	<-cleanupDone
 }
-
